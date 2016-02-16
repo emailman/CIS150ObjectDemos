8aacd61120966ed5ba493edd369b20ac8f3e2cd9
emailman <emailman@dtcc.edu>
2016-02-16 10:28:28 -0500

    added demo for wrapper class Integer

---
+++ src/p2/Main.java
@@ -0,0 +1,18 @@
+package p2;
+
+/*
+ * Created by emailman on 2/16/2016.
+ */
+
+public class Main {
+    public static void main(String[] joe) {
+
+        Integer iObject = 48;
+
+        System.out.println("object value = " + iObject);
+
+        System.out.println("result = " + Integer.parseInt("24") / 2);
+
+        System.out.println("result = " + Integer.parseInt("101",2));
+    }
+}
